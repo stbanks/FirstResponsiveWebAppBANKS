dcbcdcbccf4fe11b8b89ff2310b6930a9ad6eae5
stbanks <steven.banks@grandview.edu>
2021-05-31 22:29:26 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppBANKS.Models;
+
+namespace FirstResponsiveWebAppBANKS.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FRN = "Error";
+            ViewBag.FRAN = "0";
+            ViewBag.FRAL = "0";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FirstResponsiveModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FRN = model.GrabName();
+                ViewBag.FRAN = model.CalculateAgeNow();
+                ViewBag.FRAL = model.CalculateAgeLater();
+            }
+            else
+            {
+                ViewBag.FRN = "Error";
+                ViewBag.FRAN = "0";
+                ViewBag.FRAL = "0";
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/FirstResponsiveModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppBANKS.Models
+{
+    public class FirstResponsiveModel
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string YourName { get; set; }
+        [Required(ErrorMessage = "Please enter your birthday as mm/dd/yyyy")]
+        public DateTime Birthday { get; set; }
+        //public int AgeLater { get; set; }
+        public static DateTime Today = DateTime.Today;
+        //public DateTime EndDec = new DateTime(Today.Year, 12, 31);
+
+        public string GrabName()
+        {
+            return YourName;
+        }
+        public int CalculateAgeNow()
+        {
+            int age;
+            age = Today.Year - Birthday.Year;
+            if (DateTime.Now.DayOfYear < Birthday.DayOfYear)
+            {
+                age -= 1;
+            }
+            return age;
+        }
+        public int CalculateAgeLater()
+        {
+            int age;
+            age = Today.Year - Birthday.Year;
+            return age;
+        }
+    }
+}
